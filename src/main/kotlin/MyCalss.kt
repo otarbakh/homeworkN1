9c27ed8c803eb229f2c923259ab645cabbdf8904
otarbakh <otarbakh@gmail.com>
2022-06-16 21:09:44 +0400

    :)

--- src/main/kotlin/MyCalss.kt
+++ src/main/kotlin/MyCalss.kt
@@ -1,31 +1,39 @@
+@file:Suppress("NAME_SHADOWING")
 
 fun main() {
-
+//მივესალმე მომხამრებელს.
+    println("Hello ! ")
+//შემოვიტანე ჩემი ფუნქცია.
     Sum()
 
     println()
+//    ფუნქციის აღსრულების მერე გამოვტოვე ადგილი და მომხარებელს შევთავაზე არჩევანი კი ან არა.
     val answer:String = readLine()!!
     println()
+//    ვაილ ცილკლში გავატარე touppercase მინდოდა რო გამომეყენებინა და დამემოკლებინა , ამაზე გავჩერდი
     while (answer == "y" || answer == "Y")
     {
+//        თუ კი პასუხი აირჩია მომხმარებელმა ფუქნცია გაეშვება თავიდან
         println("${Sum()}")
-//        println()
+//        ყველაზე თავარი აქ ხდება, ვიცი არ მუშაობს სწორად მაგრამ კის შემთხვევაში რამდენჯერაც კის დავაჭერ ფუნქციის აღსრულების მერე შემომთავაზოს ხელახლა დაწყება
+        val answer:String = readLine()!!
 
     }
+//    არას შემთხვევაში მორჩა თამაში
     if (answer == "n" || answer == "N")
-
     {
-        println("დასასრული")
+        println("Finish  :)")
     }
 }
-
+//ამ ფუნქციაში ხდება რიცხვების შეტანა და დაანგარიშება.
 fun Sum()
 {
+
     print("Please assign a value for X: ")
     val numberX = readLine()!!
     print("Please assign a value for Y: ")
     val numberY = readLine()!!
     val numberZ = numberX.toInt() + numberY.toInt()
     println("The Sum is: $numberZ")
-    print("Gsurt programsis xelaxla dawyeba?: Daweret Y/N")
+    print("Do u want to restart ? :) Y/N")
 }
